1bd905a44d53bebcb27f6fc934ba8991b16abed7
Melderey <mimeev2010@gmail.com>
2021-02-04 14:20:36 +0500

    Add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import readlineSync from 'readline-sync';
 
 const giveName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
-  return;
 };
 
 export default giveName;
