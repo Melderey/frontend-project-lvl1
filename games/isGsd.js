bf48116e1713ffe21dd5f7c2267c5dd6f2cb6b8a
Melderey <mimeev2010@gmail.com>
2021-02-19 16:31:51 +0500

    Fix gsd

--- games/isGsd.js
+++ games/isGsd.js
@@ -17,7 +17,7 @@ Find the greatest common divisor of given numbers.`);
     const randomNum2 = getRandomNum(maxNum);
     const correctAnswer = gsd(randomNum1, randomNum2);
 
-    console.log(correctAnswer);
+    // console.log(correctAnswer);
 
     console.log(`Question: ${randomNum1} ${randomNum2}`);
     const answer = readlineSync.question('Your answer: ');
