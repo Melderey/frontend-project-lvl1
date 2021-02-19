09cc1c74416cc63be38650258da267eb4f92197a
Melderey <mimeev2010@gmail.com>
2021-02-19 14:58:16 +0500

    Fix answer

--- src/isAnswer.js
+++ src/isAnswer.js
@@ -1,6 +1,6 @@
 const isAnswer = (userAnswer, correctAnswer, userName) => {
   console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  return console.log(`Let's try again, ${userName}!`);
+  console.log(`Let's try again, ${userName}!`);
 };
 
 export default isAnswer;
